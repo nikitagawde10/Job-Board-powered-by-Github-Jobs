ab9667f4569f7ae3d99421c7cc7006a777359492
Nikita Gawde <gawdenikita10@gmail.com>
2020-12-02 14:44:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Heading from './Components/header';
+import Joblisting from './Components/job_listings';
+import SearchBar from './Components/search_bar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Heading/>
+     <SearchBar/>
+     <Joblisting/>
+
     </div>
   );
 }
---
+++ src/Components/header.js
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react';
+// import { render } from 'react-dom';
+
+function Heading() {
+    return <div className="Header-Box">
+        <div className="toggle">toggle</div>
+        <div className="devjobs">devjobs</div>
+    </div>
+}
+
+
+
+export default Heading
